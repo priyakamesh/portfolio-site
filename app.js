e92e7f5f12237ed7c0d79e64d2f7e2530af5908d
krishnapriya <krishnapriyakamesh@gmail.com>
2017-03-31 16:22:38 -0500

    created a basic broilerplate with different sections

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-$(".button-collapse").sideNav();
+
+$('.button-collapse').sideNav({
+     menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    });
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
